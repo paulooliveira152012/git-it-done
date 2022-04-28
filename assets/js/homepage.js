a9d74c2d169177b6a69c2ac6a13c3c7cc668dde9
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-28 11:49:29 -0400

    commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,9 @@
+/*
+1 - create a variable.
+2 - attribute an annonimous functionto this variable.
+3 - use the fetch method to get fetch objects from a server API
+4 - call this funtion
+ */
 var getUserRepos = function() {
     fetch("https://api.github.com/users/octocat/repos");
   };
