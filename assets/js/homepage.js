c7b19eb1464af7b8bbd66ac7aef5bc73527e8e90
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-29 11:18:30 -0400

    featching and 'json' repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,17 +6,16 @@
     console log the response variable
 5 - call this funtion
  */
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        //format the response in order to use the data in the code using json()
-        response.json().then(function(data){
-            console.log(data);
-        });
-        console.log("inside", response);
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
     });
-
-    var response = fetch("https://api.github.com/users/octocat/repos");
-    console.log(response);
   };
   
-  getUserRepos();
+  getUserRepos("paulooliveira152012")
