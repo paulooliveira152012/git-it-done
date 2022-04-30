8259e3259097280bf5c7425ab7f5e5e0dc5c5fd4
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-30 06:53:12 -0400

    commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,119 +1,94 @@
-/*
-1 - create a variable.
-2 - attribute an annonimous functionto this variable.
-3 - use the fetch method to get fetch objects from a server API
-4 - create a second variable for the response which equals the same link
-    console log the response variable
-5 - call this funtion
- */
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
 var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl)
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl)
     .then(function(response) {
-      //request was successful
+      // request was successful
       if (response.ok) {
+        console.log(response);
         response.json().then(function(data) {
+          console.log(data);
           displayRepos(data, user);
         });
       } else {
-        alert("Error: GitHub User Not Found");
+        alert("Error: " + response.statusText);
       }
     })
-    //adding connectivity error alert
     .catch(function(error) {
-      // Notice this `.catch()` getting chained onto the end of the `.then()` method
       alert("Unable to connect to GitHub");
     });
-  };
-  
-  
+};
 
-  // checking input field and setting up submit button ------------------------------------------------------------------------------------
-
-  //creating 2 variables, one for to refference the <form> and one for the <input>
-  var userFormEl = document.querySelector("#user-form");
-  var nameInputEl = document.querySelector("#username");
-
-  //create a function to be executed upon submission
-  var formSubmitHandler = function(event) {
-    event.preventDefault();
- 
-    // get value from input element
-    // trim() is useful if we accidentally leave a leading or trailing space in the <input> element, such as " octocat" or "octocat ".
-  var username = nameInputEl.value.trim();
-
-  //check that there's a value in that username variable.
-  if (username) {
-  getUserRepos(username);
-  nameInputEl.value = "";
-  } else {
-  alert("Please enter a GitHub username");
-  }
-    console.log(event);
-  };
-
-
-  //add event listener
-  userFormEl.addEventListener("submit", formSubmitHandler);
-
-  //displaying information to the page ------------------------------------------------------------------------------------
-  
-
-  var displayRepos = function(repos, searchTerm) {
-    // check if api returned any repos
+var displayRepos = function(repos, searchTerm) {
+  // check if api returned any repos
   if (repos.length === 0) {
-  repoContainerEl.textContent = "No repositories found.";
-  return;
+    repoContainerEl.textContent = "No repositories found.";
+    return;
   }
-    console.log(repos);
-    console.log(searchTerm);
-    // clear old content
-  repoContainerEl.textContent = "";
+
   repoSearchTerm.textContent = searchTerm;
 
-//the "fun" part... 
   // loop over repos
-for (var i = 0; i < repos.length; i++) {
-  // format repo name
-  var repoName = repos[i].owner.login + "/" + repos[i].name;
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-  // create a container for each repo
-  var repoEl = document.createElement("div");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
+    // create a link for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
-  // create a span element to hold repository name
-  var titleEl = document.createElement("span");
-  titleEl.textContent = repoName;
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
 
-  // append to container
-  repoEl.appendChild(titleEl);
+    // append to container
+    repoEl.appendChild(titleEl);
 
-  // create a status element
-  var statusEl = document.createElement("span");
-  statusEl.classList = "flex-row align-center";
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
 
-// check if current repo has issues or not
-if (repos[i].open_issues_count > 0) {
-  statusEl.innerHTML =
-    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-} else {
-  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-}
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
 
-// append to container
-repoEl.appendChild(statusEl);
+    // append to container
+    repoEl.appendChild(statusEl);
 
-  // append container to the dom
-  repoContainerEl.appendChild(repoEl);
-}
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
 
-  };
-
-  //creating two variables to refference the right column where content wil be displayed
-  var repoContainerEl = document.querySelector("#repos-container");
-  var repoSearchTerm = document.querySelector("#repo-search-term");
-
-  // User Not DFound Error ------------------------------------------------------------------------------------
+// add event listeners to forms
+userFormEl.addEventListener("submit", formSubmitHandler);
